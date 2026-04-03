3e7f5202771dc74dcfddef51398646ea91b192d4
Amartya <amartya.kumar2001@gmail.com>
2026-04-03 22:39:19 +0530

    chore: about components and elements and how to render them

--- app.js
+++ app.js
@@ -15,13 +15,29 @@ console.log(ele);
  */
 
 // Create React element using jsx syntax
-const jsxHeading = <h1 id="jsx-heading">This is a jsx syntax to create react element</h1>
+
+/**
+ const jsxHeading = <h1 id="jsx-heading">This is a jsx syntax to create react element</h1>
 
 const jsxAnchor = <a href="#">This is an anchor link</a>
 
 const jsxImg = <img src="#" alt="theres-no-img"></img>
 
 console.log(jsxHeading);
+ */
+
+const Title = () => <h1>This is the title</h1>
+
+const title = <h1>This is the title of the jsx element</h1>
+
+const HeadingComponent = () => {
+  return <div>
+    <Title />
+    {title}
+    {Title()}
+    <h1 className="heading">This is a heading element inside a heading Component</h1>
+  </div>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(jsxImg)
+root.render(<HeadingComponent />)
