3b605117f50804bf39c2532c10619a0579a8c47c
Amartya <amartya.kumar2001@gmail.com>
2026-04-03 20:37:26 +0530

    chore: jsx and babel different tags

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ const ele = React.createElement("div", { id: "container" }, [
   React.createElement("p", { className: "para", key: 2 }, "This is a paragraph...")
 ])
 
+console.log(ele);
+
 /**
  * React.createElement() is a JS object.
  * But when we render this JS object on to the DOM it becomes htmlElement
  */
 
-// Create React element using jsx
+// Create React element using jsx syntax
+const jsxHeading = <h1 id="jsx-heading">This is a jsx syntax to create react element</h1>
 
-const jsxElement = <h1 id="jsx-heading">This is a jsx syntax to create react element</h1>
+const jsxAnchor = <a href="#">This is an anchor link</a>
+
+const jsxImg = <img src="#" alt="theres-no-img"></img>
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(jsxElement)
+root.render(jsxImg)
