15bcb5eec40b8afdc31cf386488827f714a3a193
Amartya <amartya.kumar2001@gmail.com>
2026-04-04 13:18:22 +0530

    chore: make Header and Body Components

--- app.js
+++ app.js
@@ -2,42 +2,73 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-const ele = React.createElement("div", { id: "container" }, [
-  React.createElement("h1", { className: "heading-row", key: 1 }, "This is a heading"),
-  React.createElement("p", { className: "para", key: 2 }, "This is a paragraph...")
-])
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="img-element">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png" alt="app-logo"></img>
+      </div>
+      <div className="header-links">
+        <ul>
+          <li>About</li>
+          <li>Help</li>
+          <li>SignIn</li>
+          <li>Cart</li>
+        </ul>
+      </div>
 
-console.log(ele);
+    </div>
+  )
+}
 
-/**
- * React.createElement() is a JS object.
- * But when we render this JS object on to the DOM it becomes htmlElement
- */
+const ResturantCard = () => {
+  return (
+    <div className="res-card">
+      <div className="res-card-img">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/5d512d98-40e6-436d-b028-b5ccc875f059_77114.ss.jpg" alt="res-img"></img>
+      </div>
+      <div className="res-card-info">
+        <h2>Chinese Wok</h2>
+        <p>Chinese, Asian, Tibetan</p>
+        <p className="rating">⭐ 4.2 • 30–35 mins • ₹300 for two</p>
+      </div>
+    </div>
+  )
+}
 
-// Create React element using jsx syntax
+const Body = () => {
+  return (
+    <div className="res-body">
+      <div className="res-search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+        <ResturantCard />
+      </div>
+    </div>
+  )
+}
 
-/**
- const jsxHeading = <h1 id="jsx-heading">This is a jsx syntax to create react element</h1>
-
-const jsxAnchor = <a href="#">This is an anchor link</a>
-
-const jsxImg = <img src="#" alt="theres-no-img"></img>
-
-console.log(jsxHeading);
- */
-
-const Title = () => <h1>This is the title</h1>
-
-const title = <h1>This is the title of the jsx element</h1>
-
-const HeadingComponent = () => {
-  return <div>
-    <Title />
-    {title}
-    {Title()}
-    <h1 className="heading">This is a heading element inside a heading Component</h1>
-  </div>
+const AppComponent = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<HeadingComponent />)
+root.render(<AppComponent />)
